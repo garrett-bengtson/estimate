1bf5d49c1627b15822ca5da6ff1e807ab36b676d
austinpundsack <austin.pundsack@ndsu.edu>
2024-12-10 03:25:34 -0600

    Database usage for reports

--- estimate/src/main/java/edu/ndsu/cs/estimate/pages/Error404.java
+++
@@ -1,6 +0,0 @@
-package edu.ndsu.cs.estimate.pages;
-
-public class Error404
-{
-
-}
--- estimate/src/main/java/edu/ndsu/cs/estimate/services/database/interfaces/CayenneService.java
+++
@@ -1,8 +0,0 @@
-package edu.ndsu.cs.estimate.services.database.interfaces;
-
-import org.apache.cayenne.ObjectContext;
-
-public interface CayenneService {
-	ObjectContext newContext();
-}
-
